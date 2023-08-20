468569bedc6e08f591c79e86a4bae35c17bb0a45
codeto0516 <codetaisei@gmail.com>
2023-08-20 10:40:15 +0900

    devise_token_authで認証処理を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
+        before_action do
+                I18n.locale = :ja
+        end
 end
