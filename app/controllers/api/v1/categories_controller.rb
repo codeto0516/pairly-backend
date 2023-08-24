ef506067e9e9207b77bdce4cb8e17cd229072673
codeto0516 <codetaisei@gmail.com>
2023-08-25 08:12:21 +0900

    コントローラーのリファクタリング

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,16 +1,21 @@
 class Api::V1::CategoriesController < ApplicationController
 
-    def index 
-        puts "kjdlsfaskldfjsd;lfdlk;fj"
-        render json: [{
-            big_category_id: 1,
-            big_category: "未分類",
-            small_categories: [
-                {
-                    small_category_id: 1,
-                    small_category: "未分類",
-                },
-            ],
-        },]
+    def index
+        transaction_type = TransactionType.find(1)
+
+        category_list = transaction_type.big_categories.map do |big_category|
+            {
+                big_category_id: big_category.id,
+                big_category_name: big_category.name,
+                small_categories: big_category.small_categories.map do |small_category|
+                    {
+                        small_category_id: small_category.id,
+                        small_category_name: small_category.name
+                    }
+                end
+            }
+        end
+
+        render json: category_list
     end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::API
   rescue_from AuthenticationError, with: :handle_authentication_error
 
   def authenticate
-    puts "======================================================"
 
     # リクエストヘッダーからトークンを取得
     token = request.headers["Authorization"]&.split&.last
@@ -15,25 +14,16 @@ class ApplicationController < ActionController::API
     raise AuthenticationError, "not token" if token.nil?
     
     validator = FirebaseAuth::TokenValidator.new(token)
-    payload = validator.validate!
-    # raise AuthenticationError unless current_user(payload["user_id"])
-    if payload
+    @payload = validator.validate!
+
+    if @payload
       puts "認証成功！"
-      @uid = payload["user_id"]
     end
-    puts "======================================================"
-
   end
-
-  # def current_user(user_id = nil)
-  #   @current_user ||= User.find_by(uid: user_id)
-  # end
 
   private
-
-
-  def handle_authentication_error(error)
-    render json: { error: { messages: [error.message] } }, status: :unauthorized
-  end
+    def handle_authentication_error(error)
+      render json: { error: { messages: [error.message] } }, status: :unauthorized
+    end
     
 end
