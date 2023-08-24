4a2ea34f25f5935788831c2c98ea2a46be0050ca
codeto0516 <codetaisei@gmail.com>
2023-08-25 08:06:07 +0900

    マスターデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-User.create!(email: "codetaisei@gmail.com", password: "Ogawa516")
+
+# カテゴリー
+yaml_file = Rails.root.join('db/seeds', 'categories.yml')
+yaml_data = YAML.load_file(yaml_file)
+
+yaml_data.each do |data|
+  # 取引種別（支出、収入...）
+  transaction_type = TransactionType.find_or_create_by(name: data["transaction_type_name"])
+
+  data["categories"].each do |categories|
+
+    big_category = BigCategory.find_or_create_by(name: categories["big_category_name"], transaction_type_id: transaction_type.id)
+   
+    categories["small_categories"].each do |small_category_name|
+      SmallCategory.find_or_create_by(name: small_category_name, big_category_id: big_category.id)
+    end
+
+  end
+end
