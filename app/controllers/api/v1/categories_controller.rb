8ff93066c8b9a1b1a55b0b580cc049b78b1cf60f
codeto0516 <codetaisei@gmail.com>
2023-08-26 13:17:49 +0900

    新しいカテゴリーテーブルに合わせた内容に修正

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -3,19 +3,34 @@ class Api::V1::CategoriesController < ApplicationController
     def index
         transaction_type = TransactionType.find(1)
 
-        category_list = transaction_type.big_categories.map do |big_category|
+        big_cagegories = transaction_type.categories.where(parent_id: nil)
+        
+
+        category_list = big_cagegories.map do |big_cagegory|
             {
-                big_category_id: big_category.id,
-                big_category_name: big_category.name,
-                small_categories: big_category.small_categories.map do |small_category|
+                big_category_id: big_cagegory.id,
+                big_category_name: big_cagegory.name,
+                small_categories: transaction_type.categories.where(parent_id: big_cagegory.id).map do |small_category|
                     {
                         small_category_id: small_category.id,
                         small_category_name: small_category.name
                     }
                 end
+                
             }
         end
-
         render json: category_list
     end
 end
+
+
+# {
+#     big_cagegory_id: 1,
+#     big_cagegory_name: "食費",
+#     small_categories: [
+#         {
+#             small_category_id: 1,
+#             small_category_name: "食料品"
+#         }
+#     ]
+# }
