4a0480b9e0855640223c7dcde0d738de10f1df6f
codeto0516 <codetaisei@gmail.com>
2023-08-28 08:08:24 +0900

    カテゴリーモデルにchildrenを追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :parent, class_name: "Category", optional: true
+  has_many :children, class_name: "Category", foreign_key: "parent_id"
   belongs_to :transaction_type
 
   has_many :transactions
