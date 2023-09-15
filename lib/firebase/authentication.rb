125a1360cd6ca33c0444361ca5167458035543e0
codeto0516 <codetaisei@gmail.com>
2023-09-15 10:39:24 +0900

    Firebase Authentication showのキャッシュ機能 Fixes #42

--- lib/firebase/authentication.rb
+++ lib/firebase/authentication.rb
@@ -2,8 +2,10 @@ require 'json'
 require 'net/http'
 
 class Firebase::Authentication
-    FIREBASE_API_KEY = ENV.fetch('FIREBASE_API_KEY', nil)
-    FIREBASE_AUTH_DOMAIN = ENV.fetch('FIREBASE_AUTH_DOMAIN', nil)
+  FIREBASE_API_KEY = ENV.fetch('FIREBASE_API_KEY', nil)
+  FIREBASE_AUTH_DOMAIN = ENV.fetch('FIREBASE_AUTH_DOMAIN', nil)
+
+  @user_cache = {}
 
   def self.initialize_service
     service = Google::Apis::IdentitytoolkitV3::IdentityToolkitService.new
@@ -27,20 +29,34 @@ class Firebase::Authentication
   end
 
   def self.show(local_id)
+    # キャッシュを確認し、キャッシュにデータがあればそれを返す
+    return @user_cache[local_id] if @user_cache.key?(local_id)
+
+    # キャッシュにデータがなければ、APIを叩いてデータを取得する
     @service ||= initialize_service
     request = Google::Apis::IdentitytoolkitV3::GetAccountInfoRequest.new(local_id: [local_id])
     response = @service.get_account_info(request)
 
     if response.is_a?(Google::Apis::IdentitytoolkitV3::GetAccountInfoResponse)
       user_profiles = response.users
-      user_profiles.first
+      user_profile = user_profiles.first
+
+      # キャッシュにデータを保存
+      @user_cache[local_id] = user_profile
+
+      user_profile
     end
   end
 
   def self.update(local_id, display_name, photo_url)
+    # 更新データをAPIに送信
     @service ||= initialize_service
     request = Google::Apis::IdentitytoolkitV3::SetAccountInfoRequest.new(local_id:, display_name:, photo_url:)
-    response = @service.set_account_info(request)
-    response if response.is_a?(Google::Apis::IdentitytoolkitV3::SetAccountInfoResponse)
+    updated_user_profile = @service.set_account_info(request)
+
+    if updated_user_profile.is_a?(Google::Apis::IdentitytoolkitV3::SetAccountInfoResponse)
+      # 更新後にキャッシュを上書き
+      @user_cache[local_id] = updated_user_profile
+    end
   end
 end
